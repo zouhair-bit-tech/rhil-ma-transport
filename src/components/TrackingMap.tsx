6ac0cbf4a1438944e6fef67e8124418404a5e6e9
zouhair-bit-tech <zouhair.selfati@gmail.com>
2025-08-31 14:07:29 +0100

    Update TrackingMap.tsx

--- src/components/TrackingMap.tsx
+++ src/components/TrackingMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import Map from 'ol/Map';
 import View from 'ol/View';
